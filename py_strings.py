ee347d8cf5b3213c145637bb92f08ec7a9baae33
Rafał Lalik <rafal.lalik@uj.edu.pl>
2024-03-20 23:32:05 +0100

    Initial commit

---
+++ py_strings.py
@@ -0,0 +1,83 @@
+def reverse(text: str) -> str:
+    """
+    Return the 'text' backwards.
+
+    Parameters
+    ----------
+    text: str
+        The input string
+
+    Returns
+    -------
+    str
+        The text written backwards.
+    """
+    pass
+
+
+def first_to_upper(text: str) -> str:
+    """
+    Changes each first character of the word to uppercase.
+
+    Parameters
+    ----------
+    text: str
+        The input string
+
+    Returns
+    -------
+    str
+        The modified text
+    """
+    pass
+
+
+def count_vowels(text: str) -> int:
+    """
+    Counts number of vovels in the text.
+
+    Parameters
+    ----------
+    text: str
+        The input string
+
+    Returns
+    -------
+    inp
+        Number of vowels.
+    """
+    pass
+
+
+def sum_digits(text: str) -> int:
+    """
+    Finds all digitts in the text and returns its sum.
+
+    Parameters
+    ----------
+    text: str
+        The input string
+
+    Returns
+    -------
+    int
+        Sum of all digits in the text.
+    """
+    pass
+
+
+def search_substr(text: str, sub: str) -> int:
+    """
+    Search for sub(string) in the text. Returns the position or None.
+
+    Parameters
+    ----------
+    text: str
+        The input string
+
+    Returns
+    -------
+    int or None
+        Position of the sub(string) or None.
+    """
+    pass
---
+++ test_py_strings.py
@@ -0,0 +1,29 @@
+import pytest
+
+from py_strings import *
+
+
+def test_reverse():
+    assert reverse("abcd") == "dcba"
+
+
+def test_first_to_upper():
+    assert first_to_upper("abcd") == "Abcs"
+    assert first_to_upper("litwo oJCZYZNO MoJa") == "Litwo OJCZYZNO Moja"
+
+
+def test_count_vowels():
+    assert count_vowels("abcdef") == 2
+    assert count_vowels("AbcdeF") == 2
+    assert count_vowels("XYZ") == 0
+
+
+def test_sum_digits():
+    assert sum_digits("It's 911, what is your emergency?") == 11
+    assert sum_digits("3... 2... 1... kaboom!") == 6
+    assert sum_digits("Failure is not an option") == 0
+
+
+def test_search_substr():
+    assert search_substr("Litwo, Ojczyzno moja", "wo") == 3
+    assert search_substr("Litwo, Ojczyzno moja", "wa") == None
